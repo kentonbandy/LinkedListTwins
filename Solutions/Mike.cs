12c9becd461651d235b7c244137d37420e3d0533
Michael Satler <73142214+mtsatler@users.noreply.github.com>
2022-07-21 21:55:16 -0400

    Create Mike.cs
    rough first pass

---
+++ Solutions/Mike.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListTwins.Solutions
+{
+    internal static class Mike
+    {
+        // my very quickly done first pass at this just to get something working
+
+        public static int MaxTwinSum(Node node)
+        {
+            int totalNodes = 0;
+            Dictionary<int, int> hash = new Dictionary<int, int>();
+            bool endList = false;
+
+           while (!endList)
+            {
+                hash[totalNodes] = node.Val;
+                if(node.Next != null)
+                {
+                    node = node.Next;
+                    totalNodes++;
+                } else
+                {
+                    endList = true;
+                }
+            }
+
+            int maxSum = int.MinValue;
+
+            for (int i = 0; i < totalNodes / 2; i++)
+            {
+                if((hash[i] + hash[totalNodes-i]) > maxSum)
+                {
+                    maxSum = hash[i] + hash[totalNodes-i];
+                }
+            }
+
+            return maxSum;
+        }
+    }
+}
